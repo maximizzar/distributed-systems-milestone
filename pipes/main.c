e5ea99f623019d2c2fc46f2928ef1506958d2b62
Lucas <94450077+LGreubel@users.noreply.github.com>
2023-02-07 20:50:25 +0100

    bandwidth measurement gives inf for low values changed things: uses 2 pipes for interprocess communication between parent and child sends from 1 process to another through 2 pipes

--- pipes/main.c
+++ pipes/main.c
@@ -5,14 +5,18 @@
 #include <sys/types.h>
 #include <sys/time.h>
 #include <time.h>
-
-#define BUFFER_SIZE 1024
+#include <wait.h>
 
 int main(int argc, char *argv[])
 {
-    //Pipe erstellen
+    //Pipes erstellen
     int pipe_des[2];
+    int pipe_des2[2];
     if (pipe(pipe_des) < 0 ) {
+        perror("pipe error");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(pipe_des2) < 0 ) {
         perror("pipe error");
         exit(EXIT_FAILURE);
     }
@@ -26,9 +30,8 @@ int main(int argc, char *argv[])
 
     if (pid > 0) {
         //Elternprozess
-        //Schließen des nicht benötigten Eingangspipes
         close(pipe_des[0]);
-
+        close(pipe_des2[1]);
         //Latenzmessung
         char message[1];
         message[0] = 'A';
@@ -40,65 +43,89 @@ int main(int argc, char *argv[])
         //1000 Mal das Byte senden
         for (int i = 0; i < 1000; i++) {
             write(pipe_des[1], message, 1);
-            read(pipe_des[1], message, 1);
-        }
+            read(pipe_des2[0], message, 1);
 
+        }
+        printf("Parent hat 1000 mal 1 Byte gesendet und empfangen, das gegebene Byte: %c\n", message[0]);
         //Endzeit
         gettimeofday(&end, NULL);
 
         //Durchschnittliche Latenzzeit berechnen
         double latency = (end.tv_sec - start.tv_sec) * 1000.0;
         latency += (end.tv_usec - start.tv_usec) / 1000.0;
-        latency = latency/1000;
 
         //Ausgabe der Latenz
         printf("Latency: %f ms\n", latency);
 
-        //Bandbreitenmessung
-        int buffer_sizes[15] = {1024, 2048, 4096, 8192, 16384, 32758, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216};
 
+        //Sende Messwerte zurück für Bandbreite
+        for (int i = 1024; i <= (1024 * 1024) * 16; i*=2) {
+
+            //Malloc
+            char *buffer = (char *) malloc (1);
+
+            //Die Datenmenge senden
+            for (int j = 1; j <= i; j=j+ 1024) {
+                read(pipe_des2[0], buffer, i);
+                write(pipe_des[1], buffer, i);
+            }
+
+            free(buffer);
+        }
+
+        close(pipe_des[1]);
+        close(pipe_des2[0]);
     } else {
         //Kindprozess
-        //Schließen des nicht benötigten Ausgangspipes
         close(pipe_des[1]);
-
-        //Malloc zum Speichern der gelesenen Daten
-        char *buffer = (char *) malloc (BUFFER_SIZE * sizeof(char));
+        close(pipe_des2[0]);
+        //Latenzmessung
+        char message[1];
+        message[0] = 'A';
+        //1000 Mal das Byte senden
+        for (int i = 0; i < 1000; i++) {
+            read(pipe_des[0], message, 1);
+            write(pipe_des2[1], message, 1);
+        }
+        printf("Child hat 1000 mal 1 Byte gesendet und empfangen, das gegebene Byte: %c\n", message[0]);
 
         //Bandbreitenmessung
-        int buffer_sizes[15] = {1024, 2048, 4096, 8192, 16384, 32758, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216};
+        for (int i = 1024; i <= (1024 * 1024) * 16; i*=2) {
 
-        for (int i = 0; i < 15; i++) {
+            //Malloc
+            char *buffer = (char *) malloc (1);
+            //printf("I = %d\n", i);
             //Startzeit
             struct timeval start, end;
             gettimeofday(&start, NULL);
 
             //Die Datenmenge senden
-            for (int j = 0; j < buffer_sizes[i]; j++) {
-                write(pipe_des[0], buffer, buffer_sizes[i]);
-                read(pipe_des[0], buffer, buffer_sizes[i]);
+            for (int j = 1; j <= i; j=j+ 1024) {
+                write(pipe_des2[1], buffer, i);
+                read(pipe_des[0], buffer, i);
+                //      printf("J = %d\n", j);
             }
-
             //Endzeit
             gettimeofday(&end, NULL);
 
             //Durchschnittliche Bandbreite berechnen
-            double bandwith = buffer_sizes[i] * 1000.0;
-            double time = (end.tv_sec - start.tv_sec) * 1000.0;
-            time += (end.tv_usec - start.tv_usec) / 1000.0;
-            bandwith = bandwith/time;
+            double time = (end.tv_sec - start.tv_sec) * 1000;
+            time += (end.tv_usec - start.tv_usec) / 1000;
+            double bandwidth = i;
+            bandwidth = bandwidth / time;
 
             //Ausgabe der Bandbreite
-            printf("Bandwidth: %f Kbps\n", bandwith);
+            printf("ByteSize: %d KiB, Bandwidth: %f Kbit/s, %f Mbit/s\n", i / 1024, bandwidth, bandwidth / 1000.0);
+            free(buffer);
+            close(pipe_des[0]);
+            close(pipe_des2[1]);
         }
-
-        //Speicher freigeben
-        free(buffer);
+        close(pipe_des[0]);
+        close(pipe_des[1]);
+        close(pipe_des2[0]);
+        close(pipe_des2[1]);
+        wait(NULL);
     }
-
-    //Pipe schließen
-    close(pipe_des[0]);
-    close(pipe_des[1]);
 
     return 0;
 }
