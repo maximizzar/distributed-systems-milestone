ab5c95bedc4457f686cc22f2a72e2b74c21ceec7
maximizzar <maximizzar@web.de>
2023-02-07 19:29:13 +0100

    Update socket_client.c

--- sockets/socket_client.c
+++ sockets/socket_client.c
@@ -13,9 +13,11 @@
 #define BUFFERSIZE 1024
 #define PORT 8080
 
-# define KIB 1000.0
+//in bytes
+#define KIB 1024
+#define MIB 1048576
 
-int main() {
+int main(int argc,char* argv[]) {
     int sock = 0, valread;
     struct sockaddr_in address;
     int address_length = sizeof(address);
@@ -25,14 +27,13 @@ int main() {
         perror("\n Socket creation error \n");
         exit(EXIT_FAILURE);
     }
-
     memset(&address,'0',address_length);
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    if(inet_pton(AF_INET,"127.0.0.1",&address.sin_addr) <= 0) {
+    if(inet_pton(AF_INET,argv[1],&address.sin_addr) <= 0) {
         perror("\nInvalid address/ Address not supported \n");
         exit(EXIT_FAILURE);
     }
@@ -51,11 +52,7 @@ int main() {
 
     for(int i = 0; i < 1000; i++) {
         send(sock,buffer,BUFFERSIZE,0);
-        //printf("%s%d => ",buffer,i);
-        memset(buffer,0,BUFFERSIZE);
-
         read(sock,buffer,BUFFERSIZE);
-        //printf("%s\n",buffer);
     }
     gettimeofday(&end,NULL);
     memset(buffer,0,BUFFERSIZE);
@@ -63,36 +60,31 @@ int main() {
     //get mean latancy
     double latency = (end.tv_sec - start.tv_sec) * 1000.0;
     latency += (end.tv_usec - start.tv_usec) / 1000.0;
-    latency = latency/1000;
+    latency = latency / 1000;
     printf("Latency: %f ms\n\n", latency);
 
     //start bandwidth measurement..
-    int buffer_sizes[15] = {1024, 2048, 4096, 8192, 16384, 32758, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216};
+    for(int i = KIB; i <= 16 * MIB; i = i * 2) {
+        printf("File-size: %d KiB ",i / KIB);
 
-
-    for(int i = 0; i < 15; i++) {
         struct timeval start,end;
         gettimeofday(&start,NULL);
 
-        for(int j = 0; j < buffer_sizes[i]; j++) {
+        for(int j = 1; j <= i; j = j + KIB) {
             send(sock,buffer,BUFFERSIZE,0);
-            //printf("%s%d => ",buffer,i);
-            memset(buffer,0,BUFFERSIZE);
-
             read(sock,buffer,BUFFERSIZE);
-            //printf("%s\n",buffer);
         }
-
         gettimeofday(&end,NULL);
 
         //calc average bandwidth..
-        double bandwidth = buffer_sizes[i] * KIB;
-        double time = (end.tv_sec - start.tv_sec) * KIB;
-        time += (end.tv_usec - start.tv_usec) / KIB;
-        bandwidth = bandwidth/time;
+        double time = (end.tv_sec - start.tv_sec) * 1000.0;
+        time += (end.tv_usec - start.tv_usec) / 1000.0;
+
+        double bandwidth = i; //kilobyte
+        bandwidth = bandwidth / time;
 
         //print out current average bandwidth!
-        printf("%d: Bandwidth: %f kbit/s \n",i,bandwidth);
+        printf("--> bandwidth: %f KB/s\n",bandwidth);
     }
     close(sock);
     return 0;
