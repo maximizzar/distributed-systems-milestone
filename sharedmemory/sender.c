e28a9d6c522795f4f40de880b93d48eae127d799
Marvin <64065103+MarvinR1909@users.noreply.github.com>
2023-02-09 18:12:12 +0100

    update sender

--- sharedmemory/sender.c
+++ sharedmemory/sender.c
@@ -8,23 +8,27 @@
 #include <sys/time.h>
 #include <time.h>
 
+#define SHMSIZE 16777216
+
 int main()
 {
     int shmid;
     key_t key;
     char *shm;
     int semid;
+
     /* Erstelle einen Key für das Shared Memory */
     key = ftok("receiver.c", 'R');
 
-    /* Verbinde den Prozess mit dem Shared Memory */
-    shmid = shmget(key, 0, 0);
+    /* Erstelle das Shared Memory */
+    shmid = shmget(key, SHMSIZE, 0666);
     if (shmid < 0)
     {
         perror("shmget");
         return 1;
     }
 
+    /* Verknüpfe den Prozess mit dem Shared Memory */
     shm = shmat(shmid, NULL, 0);
     if (shm == (char *) -1)
     {
@@ -33,29 +37,26 @@ int main()
     }
 
     /* Verbinde den Prozess mit dem Semaphore */
-    semid = semget(key, 0, 0);
+    semid = semget(key, 1, 0);
     if (semid < 0)
     {
         perror("semget");
         return 1;
     }
 
-    struct sembuf sem_wait = {0, -1, 0};
-    struct sembuf sem_signal = {0, 1, 0};
-
-    struct timeval start, end;
+    struct sembuf sem_wait = {0, -1, 0}; /* struct für semop warten/schließen */
+    struct sembuf sem_signal = {0, 1, 0}; /* struct für semop signalisieren/öffnen */
+    struct timeval start, end; /* struct für Zeitmessung */
     
     /* Startzeit */
     gettimeofday(&start, NULL);
-
-    
+ 
     /* malloc allocate one Byte */
     char *message = (char *) malloc(1);
     message[0] = 'a';
 
-
-        /* 1024 mal senden */
-        for (int i = 0; i < 1024; i++) {
+    /* 1000 mal senden */
+    for (int i = 0; i < 1000; i++) {
 
     /* Warte auf das Semaphore */
     if (semop(semid, &sem_wait, 1) < 0)
@@ -64,8 +65,7 @@ int main()
         return 1;
     }
 
-    /* Sende ein Byte an den Server */
-    //*shm = 'a';
+    /* Sende Byte an den Receiver */
     memcpy(shm, message, 1);
 
     /* Signalisiere das Semaphore */
@@ -84,7 +84,9 @@ int main()
 
     /* Lies das Byte aus dem Shared Memory */
     char byte;
+    while(byte != 'b') {
     memcpy(&byte, shm, 1);
+    }
 
     /* Signalisiere das Semaphore */
     if (semop(semid, &sem_signal, 1) < 0)
@@ -92,7 +94,6 @@ int main()
         perror("semop signal");
         return 1;
     }
-
     //printf("%d Empfangenes Byte: %c\n",i, byte);
     }
 
@@ -105,20 +106,20 @@ int main()
     double diff = (sec + microsec / ((double) 1e6)) / 1000; /* Zeit in Sekunden für eine Messung */
     double latency = (diff * ((double) 1e3)) / 2; /* Da hin und rückweg gemessen */
 
-    printf("Latenz: %f  ms\n", latency);
+    printf("Latenz: %fms\n", latency);
 
-    int dataSize = 1024;
+////////////////// ab hier Bandbreite ///////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+    int dataSize = 1024;  
+    char *data = (char *) malloc(16777216);
 
-//////////////////////// ab hier Bandbreite /////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /* Startzeit */
-    gettimeofday(&start, NULL);
     for (int j = 0; j < 15; j++) {
 
     /* Startzeit */
     gettimeofday(&start, NULL);
-        /* 1000 mal senden */
-        for (int i = 0; i < 1000; i++) {
+
+    /* 1000 mal senden */
+    for (int i = 0; i < 1000; i++) {
 
     /* Warte auf das Semaphore */
     if (semop(semid, &sem_wait, 1) < 0)
@@ -127,7 +128,7 @@ int main()
         return 1;
     }
 
-    /* Sende Bytes an den Server */
+    /* Sende Bytes an den Sender */
     memset(shm, 'a', dataSize);
     //printf("%d Datasize %d\n", i, dataSize);
 
@@ -146,8 +147,8 @@ int main()
     }
 
     /* Lies die Byte aus dem Shared Memory */
-    char data[dataSize];
     memcpy(data, shm, dataSize);
+    //printf("%d %c\n", dataSize, data[0]);
 
     /* Signalisiere das Semaphore */
     if (semop(semid, &sem_signal, 1) < 0)
@@ -155,24 +156,24 @@ int main()
         perror("semop signal");
         return 1;
     }
-
-    //printf("%d Empfangenes Byte: %s\n",i, data);
-        }
+    //printf("%d %d Empfangenes Byte: %s\n",i, dataSize, data);
+    }
 
     /* Endzeit */
     gettimeofday(&end, NULL);
-    double sek = (end.tv_sec - start.tv_sec);
-    double microsek = (end.tv_usec - start.tv_usec) / ((double) 1e6);
-    double cache = (sek + microsek / ((double) 1e6)) / 1000;
-    double time = sek + microsek;
 
      /* Durchschnittliche bandbreite */
-    double time2 = (end.tv_sec - start.tv_sec);
-    time2 += (end.tv_usec - start.tv_usec) / ((double) 1e6);
-    double bandwidth = dataSize / time2;
-    printf("Zeit: %fs Bandbreite %.2fKB/s Bytes: %d\n", time, bandwidth, dataSize);
+    double time = (end.tv_sec - start.tv_sec);
+    time += (end.tv_usec - start.tv_usec) / ((double) 1e6); /* mikrosekunden in sekunden umrechnen */
+    double bandwidth;
+    bandwidth = dataSize / time;
+    bandwidth /= 1000; /* Durchschnittliche Bandbreite für eine Messung */
+    printf("Zeit: %fs Bandbreite: %.2fKB/s Bytes: %d\n", time, bandwidth, dataSize);
 
     dataSize *= 2;
     }
+    shmdt(shm); /* Shared Memory von prozess entfernen */
+    shmctl(shmid, IPC_RMID, NULL); /* Shared Memory löschen */
+    semctl(semid, 0, IPC_RMID); /* Semaphore löschen */
     return 0;
 }
